a5a2fe38bbff70d4cf423a1535399281a27ca690
monkey5173 <sonseongtae@gmail.com>
2024-12-23 11:28:26 +0900

    Add CSPJ
    진행중이던 프로젝트 추가했음 이제 깃허브에서 기능 추가해가면서 작성할 예정

---
+++ Screen.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+struct Vector2
+{
+    int _x;
+    int _y;
+
+    public int X { get; set; }
+    public int Y { get; set; }
+    
+}
+
+class Screen
+{
+    int _width = 0;
+    int _height = 0;
+    int _roadWidth = 0;
+    Char[,] wall;
+
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int RoadWidth { get; set; }
+
+    public Screen()
+    {
+        Width = 60; //가로 넓이
+        Height = 30; //세로 넓이
+        RoadWidth = 10; //길의 넓이
+
+    }
+
+    public void SetWall()
+    {
+        wall = new char[Height, Width];
+        int LeftEdge = (Width - RoadWidth) / 2;
+        int RightEdge = LeftEdge + RoadWidth;
+
+        for(int i = 0; i < Height; i++)
+        {
+            for(int j = 0; j < Width; j++)
+            {
+                if(j < LeftEdge || j > RightEdge)
+                {
+                    wall[i, j] = 'ο';
+                }
+                else
+                {
+                    wall[i, j] = ' ';
+                }
+            }
+        }
+    }
+    
+    public void DrawWall()
+    {
+        for(int i = 0; i < Height; i++)
+        {
+            for(int j = 0; j < Width; j++)
+            {
+                Console.Write(wall[i,j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
