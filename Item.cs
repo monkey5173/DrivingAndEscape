86074e5b48ca1653cefcd729e8b742883061c4d4
Seong_Tae_SON <sonseongtae@gmail.com>
2024-12-25 20:25:42 +0900

    이것저것 조정중
    랜덤한 X축 위치에 아이템이 생성되고 아래로 떨어질 때 플레이어가 그 아이템에 맞으면 사망하는 시스템을 구상하고 의도대로 구현되게 시도해보고 있는데 생각대로 안되서 고쳐보는중

---
+++ Item.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sketch
+{
+    class Item
+    {
+        Vector2 itemPos;
+        char[] _itemType;
+
+        public char[] ItemType
+        {
+            get { return _itemType; }
+            set { _itemType = value; }
+        }
+
+        public Item() 
+        { 
+            itemPos = new Vector2();
+            ItemType = new char[3] {'★', '♥', '■'};
+        }
+
+        public void DropItems(Player player, Screen screen, Game game)
+        {
+            Random posRandom = new Random();
+            itemPos.X = posRandom.Next(0, screen.Width);
+            int randomType = posRandom.Next(0, 3);
+
+            if (game.GameIsPlaying)
+            {
+                for (int i = 0; i < screen.Height; i++)
+                {
+                    Console.SetCursorPosition(itemPos.X, itemPos.Y);
+                    Console.Write(' ');
+
+                    itemPos.Y++;
+
+                    if(itemPos.Y < screen.Height - 1)
+                    {
+                        Console.SetCursorPosition(itemPos.X, itemPos.Y);
+                        Console.Write(ItemType[randomType]);
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(itemPos.X, itemPos.Y);
+                        Console.Write(' ');
+                        break;
+                    }
+                }
+                    
+
+            }
+        }
+    }
+}
